a4b4b869bf10f3629230dcde47e282ad503f0977
wiktoriakeller <wiktoriakeller552@gmail.com>
2021-11-18 00:21:50 +0100

    Coloring white pits

--- diceRecognition.py
+++ diceRecognition.py
@@ -98,8 +98,7 @@ def findContoursInDice(img):
     diceImgGray = cv.cvtColor(dices[i], cv.COLOR_BGR2GRAY)
 
     diceImgBlur = cv.GaussianBlur(diceImgGray, (3, 3), 50, 50)
-    diceThreshold = cv.threshold(diceImgBlur, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU) 
-    diceImgThreshold = diceThreshold[1]
+    diceImgThreshold = cv.threshold(diceImgBlur, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)[1]
 
     diceImgCanny = cv.Canny(diceImgThreshold, 20, 255)
     contours, hierarchy = cv.findContours(diceImgCanny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
@@ -127,6 +126,9 @@ def simpleBlobDetection(img, minThreshold, maxThreshold, minArea, maxArea, minCi
     return cv.drawKeypoints(img, keypoints, np.array([]), (0, 0, 255),
                 cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS), len(keypoints)
 
+def deleteGlares(img):
+    pass
+
 #blob detection parameters
 minThreshold = 50                  
 maxThreshold = 200     
@@ -141,8 +143,7 @@ blank = np.zeros_like(img)
 imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 imgBlur = cv.GaussianBlur(imgGray, (3, 3), 10)
 
-threshold = cv.threshold(imgBlur, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU) 
-imgThreshold = threshold[1]
+imgThreshold = cv.threshold(imgBlur, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)[1]
 
 kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
 imgThreshold = cv.morphologyEx(imgThreshold, cv.MORPH_CLOSE, kernel)
@@ -163,6 +164,20 @@ imgArea = dices[0].shape[0] * dices[0].shape[1]
 maxArea = int(imgArea / 2)
 
 for i in range(len(dices)):
+    blur = cv.medianBlur(dices[i], 5)
+    hsv = cv.cvtColor(blur, cv.COLOR_BGR2HSV)
+    lowerGray = np.array([0, 5, 50], np.uint8)
+    upperGray = np.array([179, 50, 255], np.uint8)
+    maskGray = cv.inRange(hsv, lowerGray, upperGray)
+    mean = np.mean(maskGray)
+
+    for j in range(maskGray.shape[0]):
+        for g in range(maskGray.shape[1]):
+            if(maskGray[j][g] >= 220 and mean <= 30):
+                dices[i][j][g][0] = 0
+                dices[i][j][g][1] = 0
+                dices[i][j][g][2] = 0
+
     imgWithKeypoints, number = simpleBlobDetection(dices[i], minThreshold, maxThreshold, minArea, maxArea, minCircularity, minInertiaRatio)
     cv.putText(imgWithKeypoints, str(number), (5, 25), cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
     filteredDices.append(imgWithKeypoints)
